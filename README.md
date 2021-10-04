9b9f09672742fd1e7bbf8cfafe3e43fd011d28b6
Chase <Gcrowder93@gmail.com>
2021-10-04 16:17:01 -0700

    ive commited like 5 times without adding, so this is a big commit

--- README.md
+++ README.md
@@ -2,24 +2,38 @@
 
 ## Making a plan
 
-(bolded steps are mandatory, unbolded are for more advanced projects)
+### Html Element
+* header -- static
+* user input -- static
+* button -- static
+user message -- dynamic and change based on the user gyess
 
-1) **Make a drawing of your app. Simple "wireframes"**
-2) **Once you have a drawing, name the HTML elements you'll need to realize your vision**
-3) **For each HTML element ask: Why do I need this?**
-4) Ask which of out HTML elements are hard coded, and which are dynamically generated?
-5) **Once we know _why_ we need each element, think about how to implement the "Why" as a "How"**
-6) Is there some state we need to initialize?
-7) **Find all the 'events' (user clicks, form submit, on load etc) in your app. Ask one by one, "What happens when" for each of these events. Does any state change?**
-8) **Think about how to validate each of your steps**
-9) Ask: should any of this work be abstracted into functions? (i.e., is the work complicated? can it be resused?)
-10) Consider your data model. What objects will you be using? What are the key/value pairs? What arrays do you need? What needs to live in local storage?
-11) **Consider what features _depend_ on what other features. Use this dependency logic to figure out what order to complete tasks.**
+### State
+* userGuess -- will hold the value of the input field
+* randomNum -- will be generate at the start of every game
+* numGuesses -- number of guesses -- incremented every time the user clicks the button
+* userMessage
+     -'You're correct': userGuess === randomNum
+     -'Guess too high': userGuess > randomNum
+     'Guess too low': userGuess < randomNum
+     'No more guesses': numGuesses > 4
+     -'Invalid guess': if input is >20 or not a number(stretch)
 
+### PsuedoCode for Event
+```javascript
+// what DOM elements needed
 
-## To Run Cypress Tests
-* `npm install`
-* `npm test`
-* Cypress will open -- you should then click "run <#> integration spec(s)"
-    ![](cypress.png)
-* Make sure all tests pass
+// what state do i need
+
+// what events am i listening for
+// on button click do the following:
+// decrement or increment the guess count
+// get the value of the user guess from the input
+// CONVERT INPUT TO A NUNMBER
+// compare user guess to randomNumber
+// if(userGuess === randomNum) -- display you won!
+// else if (numGuesses === 4 ) --display you lost, out og guesses
+// else if (userGuess > randomNum) -- display guess too high!
+// melse if (userGuess < randomNum) -- display guess too low
+```
+
--- app.js
+++ app.js
@@ -1,8 +1,97 @@
-// import functions and grab DOM elements
 
-// initialize global state
+let randomNumber = Math.floor(Math.random() * 20) + 1;
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+const guesses = document.querySelector('.guesses');
+const lastResult = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+
+const guessSubmit = document.querySelector('.guessSubmit');
+const guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+guessField.focus();
+
+
+function checkGuess() {
+  let userGuess = Number(guessField.value);
+  if (guessCount === 1) {
+    guesses.textContent = 'Previous guesses: ';
+  }
+  guesses.textContent += userGuess + ' ';
+
+  if (userGuess === randomNumber) {
+    lastResult.textContent = 'Nailed it!';
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else if (guessCount === 4) {
+    lastResult.textContent = 'You lost';
+    lowOrHi.textContent = '';
+    setGameOver();
+  } else {
+    lastResult.textContent = 'Nope! Guess Again..';
+    lastResult.style.backgroundColor = 'yellow';
+    if(userGuess < randomNumber) {
+      lowOrHi.textContent = 'Thats too low';
+      lowOrHi.style.backgroundColor = 'peachpuff'
+    } else if(userGuess > randomNumber) {
+      lowOrHi.textContent = 'Thats too high!';
+      lowOrHi.style.backgroundColor = 'tomato'
+    }
+  }
+  
+  guessCount++;
+  guessField.value = '';
+  guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+
+function setGameOver() {
+  guessField.disabled = true;
+  guessSubmit.disabled = true;
+  resetButton = document.createElement('button');
+  resetButton.textContent = 'Start new game';
+  resetButton.id= 'resetbutton';
+  document.body.append(resetButton);
+  resetButton.addEventListener('click', resetGame);
+}
+
+
+function resetGame() {
+  guessCount = 1;
+  
+
+  const resetParas = document.querySelectorAll('.resultParas p');
+  for (let i = 0 ; i < resetParas.length ; i++) {
+    resetParas[i].textContent = '';
+  }
+
+  resetButton.parentNode.removeChild(resetButton);
+
+
+  guessField.disabled = false;
+  guessSubmit.disabled = false;
+  guessField.value = '';
+  
+
+  lastResult.style.backgroundColor = 'white';
+
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+const resetParas = document.querySelectorAll('.resultParas p');
+for (let i = 0 ; i < resetParas.length ; i++) {
+  resetParas[i].textContent = '';
+}
+
+guesses.style.boxShadow = '2px 2px 6px black';
+lastResult.style.boxShadow = '3px 3px 6px black';
+lowOrHi.style.boxShadow = '3px 3px 6px black';
+
+
+
+
+
